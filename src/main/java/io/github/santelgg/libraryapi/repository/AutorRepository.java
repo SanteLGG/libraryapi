4a09383ec56bbef0d3d200d1833e170f9e7dff7a
luisguilherme <l.g.g.sante@gmail.com>
2025-09-15 13:02:48 -0300

    criação das repositorys

---
+++ src/main/java/io/github/santelgg/libraryapi/repository/AutorRepository.java
@@ -0,0 +1,8 @@
+package io.github.santelgg.libraryapi.repository;
+
+import io.github.santelgg.libraryapi.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface AutorRepository extends JpaRepository<Autor, UUID> {
+}
